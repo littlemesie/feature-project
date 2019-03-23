7b3e9a4db868026068e6a3d7f9d3075a2b29dfb9
littlemesie <littlemesie@163.com>
2019-03-23 16:22:14 +0800

    update 目录

--- src/feature_tools/pca.py
+++ src/feature_tools/pca.py
@@ -1,16 +1,15 @@
 # -*- coding:utf-8 -*-
-import pandas as pd
-import numpy as np
-from sklearn.decomposition import PCA
 
 from feature_tools import generate_feature
 
 def pca():
     """使用PCA进行降维"""
     features, feature_names = generate_feature.convert()
-    ipca = PCA(n_components=10, copy=True)
-    data = ipca.fit_transform(features)
+
+    # ipca = PCA(n_components=10, copy=True)
+    # data = ipca.fit_transform(features)
     # da = ipca.inverse_transform(data)
-    print(ipca.score_samples(features))
+    # print(ipca.score_samples(features))
+    print(features.corr())
 
 pca()
